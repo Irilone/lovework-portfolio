0235bea738feb66f668501631936929cc958feb5
Dorian <avillus@chalmers.se>
2025-02-20 12:44:23 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    allowedHosts: [
+      'f4165080-5adc-4341-8b4c-e25a17f06f03.lovableproject.com'
+    ]
+  }
+});
