efc64c94de403fac86f9776914691d3de36308e1
Dorian <avillus@chalmers.se>
2025-02-20 13:47:44 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 8080,
